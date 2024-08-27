9d73457877a50c5371d8164d3a7fe6570a7160e8
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-08-27 15:32:59 +0000

    login endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -72,3 +72,21 @@ def handle_fan(fans_id):
         response_body['results'] = row.serialize()
         response_body['message'] = f'recibi el get request {fans_id}'
         return response_body, 200
+
+
+@api.route('/login', methods=["POST"])
+def login():
+    response_body = {}
+    data = request.josn 
+    email = data.get('email', None).lower()
+    password = data.get('password', None)
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalar()
+    if not user:
+        response_body['message'] = 'Something is wrong, check your email/password or user innactive'
+        return response_body, 401
+    acces_token = create_access_tokes(identify={'email': user.email,
+                                                'user_id': user.id,})
+    response_body['results'] = user.serialize()
+    response_body['message'] = 'user logged'
+    response_body['access_token'] = access_token
+    return response_body, 201
