3dcb9425325dd88a30ea60b489ea01dd80cf5504
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-08-27 17:39:03 +0000

    cambios flask_jwt

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,10 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users, Artists, Covers, Comments, Fans, Follows
+from api.models import db, Users, Artists, Covers, Comments, Fans, Follows, Songs
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
 
 
 api = Blueprint('api', __name__)
@@ -64,16 +67,16 @@ def handle_fans():
 def handle_fan(fans_id):
     response_body = {}
     if request.method == 'GET':
-        row = db.session.execute(db.select(Fans).where(Fans.id == fans_id)).scalar()
+        row = db.session.execute(db.select(Fans).where(Fans.id == fans_id)).scalars()
         if not row:
             response_body['results'] = {}
             response_body['message'] = f'No existe el fan {fans_id}'
-            return response_body, 400
+            return response_body, 404
         response_body['results'] = row.serialize()
         response_body['message'] = f'recibi el get request {fans_id}'
         return response_body, 200
     if request.method == 'PUT':
-        response_body['message'] = f'recibí eñ PUT request {fans_id}'
+        response_body['message'] = f'recibí el PUT request {fans_id}'
         return response_body, 200
     if request.method == 'DELETE':
         response_body['message'] = f'recibí el DELETE request {fans_id}'
@@ -86,13 +89,20 @@ def login():
     data = request.josn 
     email = data.get('email', None).lower()
     password = data.get('password', None)
-    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalar()
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalars()
     if not user:
         response_body['message'] = 'Something is wrong, check your email/password or user innactive'
         return response_body, 401
-    acces_token = create_access_tokes(identify={'email': user.email,
+    acces_token = create_access_tokes(identify={'email': email,
                                                 'user_id': user.id,})
     response_body['results'] = user.serialize()
-    response_body['message'] = 'user logged'
+    response_body['message'] = 'User logged'
     response_body['access_token'] = access_token
     return response_body, 201
+
+
+@api.route('/songs', methods=['GET'])
+def handle_songs():
+    response_body = {}
+    if request.method == 'GET':
+        rows = db.session.execute(db.select(Songs)).scalars()
