dc25328747bc9ebd92d91544506152ebeaf8732c
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-09-01 19:12:52 +0000

    arreglos-4

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,7 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
-from api.models import db, Users, Artists, Covers, Comments, Fans, Follows, Songs
+from api.models import db, Users, Artists, Covers, Comments, Fans, Follows, Songs, Votes
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import jwt_required
