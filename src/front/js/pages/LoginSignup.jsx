2f1b6967881d19d6516e9e1646e8300e728ee9f5
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-09-09 22:50:33 +0000

    loginsignup

--- src/front/js/pages/LoginSignup.jsx
+++ src/front/js/pages/LoginSignup.jsx
@@ -1,4 +1,6 @@
 import React, {useContext, useState} from "react";
+import logo from '../../img/Soundex_Isotipo_SVG.png';
+import size_img from '../../img/imagen-login.jpg';
 
 export const LoginSignup = () => {
 
@@ -12,39 +14,45 @@ const [action,setAction] = useState('Sign Up');
 return (
     <div className='Container-fluid'>
         <div className="row">
-            <div className="col-md-6">{/* image */}</div>
             <div className="col-md-6">
-                <div className="card">
-                    <div className="card-body">
-                    <h2 className="card-title text-center mb-3 display-5">{action}</h2>
-                    <div className="underline"></div>
-                    <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <label htmlFor="email" className="mb-1">email</label>
-                        <input type="email" className="form-control" id="email" required/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password" className="mb-1">Password</label>
-                        <input type="password" className="form-control" id="password" required/> 
-                    </div>
-                    <div className="form-group">
-                    {action === 'Login'?<div></div>:<div><label htmlFor="inputRol" className="form-label">Rol</label>
-                        <select id="inputRol" className="form-select">  
-                        <option defaultValue>Here you can choose your rol</option>
-                        <option value="1">Artist</option>
-                        <option value="2">Fan</option>
-                        </select>
-                        </div>}
-                    </div>
-                    <div className="submit-container text-center">
-                        <button type="button" className={action === 'Login'?'btn-secondary':'submit'} onClick={()=>{setAction('Sign Up')}}/* "btn btn-primary mt-5" */ >Sign Up</button>
-                        <button type="button" className={action === 'Sign Up'?'btn-secondary':'submit'} onClick={()=>{setAction('Login')}}/* "btn btn-primary mt-5" */ >Log In</button>
-                    </div>
-                    </form>
-                </div>
+            <img src={size_img} style={{ width: '85vh', height: '100vh' }}></img>
             </div>
+                <div className="col-md-6 right-side">
+                    <div className="card-login" style={{ width: '25rem', display: 'flex', justifyContent: 'center', alignItems: 'center',height: '50vh', marginTop: '50px', borderRadius:'15px'}}>
+                        <div className="card-body">
+                            <div className="" style={{display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
+                            <img src={logo} alt="" style={{width: '80px', marginRight: '50px'}}/>
+                            <h2 className="card-title text-center">{action}</h2>
+                            </div>
+                            <div className="underline"></div>
+                            <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="email" className="mb-1">email</label>
+                                <input type="email" className="form-control" id="email" required/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password" className="mb-1">Password</label>
+                                <input type="password" className="form-control" id="password" required/> 
+                            </div>
+                            <div className="form-group">
+                            {action === 'Login'?<div></div>:<div><label htmlFor="inputRol" className="form-label">Rol</label>
+                                <select id="inputRol" className="form-select">  
+                                <option defaultValue>Here you can choose your rol</option>
+                                <option value="1">Artist</option>
+                                <option value="2">Fan</option>
+                                </select>
+                                </div>}
+                            </div>
+                            <div className="submit-container text-center">
+                                <button type="button" className={action === 'Login'?'btn-secondary':'submit'} onClick={()=>{setAction('Sign Up')}}/* "btn btn-primary mt-5" */>Sign Up</button>
+                                <button type="button" className={action === 'Sign Up'?'btn-secondary':'submit'} onClick={()=>{setAction('Login')}}/* "btn btn-primary mt-5" */>Log In</button>
+                            </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 
-);
+    );
 };
