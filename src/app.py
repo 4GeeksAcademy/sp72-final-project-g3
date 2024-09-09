da0f0a213970c2f9cc121775ea5c2f69c0a06765
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-09-09 15:56:37 +0000

    spotify_endpoint

--- src/app.py
+++ src/app.py
@@ -40,38 +40,6 @@ jwt = JWTManager(app)
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-def get_token():
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
-    url = 'https://accounts.spotify.com/api/token'
-    headers = {'Authorization': 'Basic ' + auth_base64,
-                'Content_Type': 'application/x-www-form-urlencoded'}
-    data = {"grant_type" : "client_credentials"}
-    result = requests.post(url, headers=headers, data=data)
-    json_result = json.loads(result.content)
-    token = json_result['access_token']
-    print(token)
-    return token
-
-
-""" def get_auth_header(token): 
-    return {'Autorization': 'Bearer ' + token}"""
-
-def search_for_artist(artist_name):
-    token = get_token()
-    url = 'https://api.spotify.com/v1/search'
-    # headers = get_auth_header(token)
-    headers = {'Authorization': 'Bearer ' + token}
-    query = f'?q={artist_name}&type=artist&limit=1'
-    query_url = url + query
-    result = requests.get(query_url, headers=headers)
-    print(result)
-    json_result = json.loads(result.content)
-    print(json_result)
-
-search_for_artist('ADELE')
-
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
