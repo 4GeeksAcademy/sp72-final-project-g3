553b8265c00d67822b091df5b440d0f3e6cfaa54
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-09-13 21:49:20 +0000

    [flux} feat:agregando APIs

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,7 +1,24 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			currentUser: null,
 			message: null,
+			rol: {
+				fan: [],
+				artist: [],
+				admin: [],
+			},
+			users: [],
+			user: [],
+			login: [],
+			signup: [],
+			covers: [],
+			cover:[],
+			songs: [],
+			song: [],
+			spotify: [],
+			comments: null,
+			image: []
 		},
 		actions: {
 			getMessage: async () => {
@@ -14,7 +31,127 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}
 					const data = await response.json()
 					setStore({ message: data.message })
-					return data;
+					return data;},
+			postImage: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/upload`
+				const options = {
+					method: "POST"
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("you got some backend problems", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({image: data.image})
+				return data;
+			},
+			getUsers: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/users`
+				const options = {
+					method: "GET"
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({users: data.users})
+			},
+			getUser: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/users/${user_id}`
+				const options = {
+					method: "GET"
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({user: data.user})
+			},
+			login: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/login`
+				const options = {
+					method: "POST",
+					headers:{
+						'content-Type': 'aplication/json',
+						"Authorization": `Bearer ${token}`
+					}
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({
+					user: data.user,})
+			},
+			signUp: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/signup`
+				const options = {
+					method: "POST",
+					headers:{
+						'content-Type': 'aplication/json',
+						"Authorization": `Bearer ${token}`
+					}
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({
+					user: data.user,
+					rol: data.rol
+				})
+			},
+			getArtists: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/artists`
+				const options = {
+					method: "GET"
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({artists: data.artists})
+			},
+			getArtist: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/users/${artist_id}`
+				const options = {
+					method: "GET"
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({artist: data.artist})
+			},
+			editArtist: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/users/${artist_id}`
+				const options = {
+					method: "PUT",
+					headers:{
+						'content-Type': 'aplication/json',
+						"Authorization": `Bearer ${token}`
+					}
+				}
+				const response = await fetch(uri, options)
+				if (!response.ok){
+					console.log("WTF nothing to see here", response.status, response.statusText);
+					return
+				}
+				const data = await response.json()
+				setStore({artist: data.artist})				
 			},
 		}
 	};
