6b74ceac4bb66b05eb291e6349917dae01b9735a
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-09-17 18:33:09 +0000

    Flux cloudnary completed

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -1,22 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext.js";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import { CoverCard } from "../component/CoverCard.jsx";
 import { HeroSection } from "../component/HeroSection.jsx";
+import { ImageUploader } from "../component/ImageUploader.jsx";
 
 
 export const Home = () => {
+	const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
+	const token = 'your-auth-token';
 	const { store, actions } = useContext(Context);
+
+	const handleUploadSuccess = (imageUrl) => {
+		setUploadedImageUrl(imageUrl);
+	}
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
 			<HeroSection />
 			<CoverCard />
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<div>
+				<h1>Perfil de Usuario</h1>
+				< ImageUploader token={token} onUploadSuccess={handleUploadSuccess} />
+				{uploadedImageUrl && (
+					<div>
+						<h2>Artwork del cover</h2>
+						<img src={uploadedImageUrl} alt="Uploaded image" />
+					</div>
+				)}
+			</div>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -13,118 +13,132 @@ const getState = ({ getStore, getActions, setStore }) => {
 			login: [],
 			signup: [],
 			covers: [],
-			cover:[],
+			cover: [],
 			songs: [],
 			song: [],
 			spotify: [],
 			comments: null,
-			image: []
+			image: null,
+			uploadStatus: null
 		},
 		actions: {
-			getMessage: async () => {
-					const uri = `${process.env.BACKEND_URL}/api/hello`
-					const options = {headers: {'Content-types': 'application/json'}}
-					const response = await fetch(uri, options)
-					if (!response.ok) {
-						console.log("Error loading message from backend", response.status, response.statusText);
-						return;
-					}
-					const data = await response.json()
-					setStore({ message: data.message })
-					return data;},
-			postImage: async () => {
-				const uri = `${process.env.BACKEND_URL}/api/upload`
-				const options = {
-					method: "POST"
-				}
+			/*getMessage: async () => {
+				const uri = `${process.env.BACKEND_URL}/api/hello`
+				const options = { headers: { 'Content-types': 'application/json' } }
 				const response = await fetch(uri, options)
-				if (!response.ok){
-					console.log("you got some backend problems", response.status, response.statusText);
-					return
+				if (!response.ok) {
+					console.log("Error loading message from backend", response.status, response.statusText);
+					return;
 				}
 				const data = await response.json()
-				setStore({image: data.image})
+				setStore({ message: data.message })
+				return data;
+			},*/
+			postImage: async (imageFile) => {
+				const uri = `${process.env.BACKEND_URL}/api/upload`;
+				const formData = new FormData();
+				formData.append('image', imageFile);  // 'file' es el nombre del campo esperado por el backend
+				const options = {
+					method: "POST",
+					headers: {
+						'Authorization': `Bearer ${token}`
+					},
+					body: formData
+				};
+				const response = await fetch(uri, options);
+				if (!response.ok) {
+					console.log("you got some backend problems", response.status, response.statusText);
+					return;
+				}
+				const data = await response.json();
+				setStore({ image: data.results });
 				return data;
 			},
 			getUsers: async () => {
 				const uri = `${process.env.BACKEND_URL}/api/users`
 				const options = {
-					method: "GET"
+					method: "GET",
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({users: data.users})
+				setStore({ users: data.result })
 			},
 			getUser: async () => {
 				const uri = `${process.env.BACKEND_URL}/api/users/${user_id}`
 				const options = {
-					method: "GET"
+					method: "GET",
+					headers: {
+						'Content-Type': 'application/json'
+					}
 				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({user: data.user})
+				setStore({ user: data.result })
 			},
 			login: async (email, password) => {
-				const dataToSend = {email, password}
+				const dataToSend = { email, password }
 				const uri = `${process.env.BACKEND_URL}/api/login`
 				const options = {
 					method: "POST",
 					body: JSON.stringify(dataToSend),
 					headers: {
-					  'Content-Type': 'application/json',
+						'Content-Type': 'application/json',
 					}
-				  }				  
+				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({currentUser: data.results})
+				setStore({ currentUser: data.results })
 				localStorage.setItem("token", data.access_token);
 				localStorage.setItem("user", data.results);
 			},
 			signUp: async (email, password, rol) => {
-				const dataToSend = {email, password, rol}
+				const dataToSend = { email, password, rol }
 				const uri = `${process.env.BACKEND_URL}/api/signup`
 				const options = {
 					method: "POST",
 					body: JSON.stringify(dataToSend),
 					headers: {
-					  'Content-Type': 'application/json',
+						'Content-Type': 'application/json',
 					}
-				  }				  
+				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({currentUser: data})
+				setStore({ currentUser: data })
 				localStorage.setItem("token", data.access_token);
 				localStorage.setItem("user", data.results);
 			},
 			getArtists: async () => {
-				
+
 				const uri = `${process.env.BACKEND_URL}/api/artists`
 				const options = {
 					method: "GET"
 				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({artists: data.artists})
+				setStore({ artists: data.artists })
 			},
 			getArtist: async () => {
 				const uri = `${process.env.BACKEND_URL}/api/users/${artist_id}`
@@ -132,46 +146,46 @@ const getState = ({ getStore, getActions, setStore }) => {
 					method: "GET"
 				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({artist: data.artist})
+				setStore({ artist: data.artist })
 			},
 			editArtist: async () => {
 				const uri = `${process.env.BACKEND_URL}/api/users/${artist_id}`
 				const options = {
 					method: "PUT",
-					headers:{
+					headers: {
 						'content-Type': 'aplication/json',
 						"Authorization": `Bearer ${token}`
 					}
 				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({artist: data.artist})				
+				setStore({ artist: data.artist })
 			},
 			deleteArtist: async () => {
 				const uri = `${process.env.BACKEND_URL}/api/users/${artist_id}`
 				const options = {
 					method: "DELETE",
-					headers:{
+					headers: {
 						'content-Type': 'aplication/json',
 						"Authorization": `Bearer ${token}`
 					}
 				}
 				const response = await fetch(uri, options)
-				if (!response.ok){
+				if (!response.ok) {
 					console.log("WTF nothing to see here", response.status, response.statusText);
 					return
 				}
 				const data = await response.json()
-				setStore({artist: data.artist})
+				setStore({ artist: data.artist })
 			},
 
 		}
