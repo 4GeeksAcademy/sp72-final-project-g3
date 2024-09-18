2c9948111e5090453db867f2cc12336cd3b475a6
Marcel Rivero <126285355+Marci25@users.noreply.github.com>
2024-09-18 08:22:57 +0000

    hay que alinear las cards de review

--- src/front/js/component/ReviewCard.jsx
+++ src/front/js/component/ReviewCard.jsx
@@ -8,7 +8,7 @@ import { Context } from '../store/appContext.js';
 
 export const ReviewCard = () => {
 
-    const { store } = useContext(Context)
+    const { store, actions } = useContext(Context)
 
 
     const VotesRating = ({ votes }) => {
@@ -29,28 +29,30 @@ export const ReviewCard = () => {
 
 
     return (
-        <div className="card card-review" style={{ width: '25rem' }} >
-            <div className="card-body">
-                {store.comments.map((item) =>   
-                    <div key={item.id}> 
-                    <VotesRating rating= '5' /> 
-                    <h5 className="card-title-review text-white">{item.title}</h5>
-                    <p className="card-text-review">{item.body}</p>
-                    <div className="d-flex align-items-center mt-4">
-                        <img
-                            src={rigo}
-                            alt="Profile"
-                            className="rounded-circle"
-                            style={{ width: '50px', height: '50px', objectFit: 'cover' }}
-                        />
-                        <div className="ms-3">
-                            <h6 className="text-white mb-0">{item.user_id}</h6>
-                            <small className="text-secondary">{item.created_at}</small>
+        <div className="review-card-container" >
+                <div className="card-body">
+                    {store.mostcomments.map((item) =>
+                        <div key={item.id} className="card card-review" >
+                            <div className="card-body" >
+                            <VotesRating rating='5' />
+                            <h5 className="card-title-review text-white">{item.title}</h5>
+                            <p className="card-text-review">{item.body}</p>
+                            <div className="d-flex align-items-center mb-4 mt-4">
+                                <img
+                                    src={rigo}
+                                    alt="Profile"
+                                    className="rounded-circle"
+                                    style={{ width: '50px', height: '50px', objectFit: 'cover' }}
+                                />
+                                <div className="ms-3">
+                                    <h6 className="text-white mb-0">{item.user_id}</h6>
+                                    <small className="text-secondary">{item.created_at}</small>
+                                </div>
+                            </div>
+                            </div>
                         </div>
-                    </div>
+                    )}
                 </div>
-            )}
-            </div>
         </div>
     );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -22,6 +22,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 			spotify: [],
 			votes: [],
 			comments: [],
+			mostcomments: [],
 			image: []
 		},
 		actions: {
@@ -173,11 +174,20 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 				const response = await fetch(uri, options)
 				if (!response.ok){
-					console.log('Error loading message from backend", reponse.status, response.statusText')
+					console.log('Error loading message from backend", response.status, response.statusText')
 					return
 				}
 				const data = await response.json();
 				setStore({ comments: data.results })
+				if (getStore().comments.length <= 3){
+					setStore({mostcomments: data.results})
+				} else {
+					const lastIndex = Math.floor(Math.random() * (getStore().comments.length -2)) + 2;
+					const mostcomments = [data.results[0], data.results[1], data.results[lastIndex]];
+					setStore({mostcomments: mostcomments})
+
+				}
+			
 			},
 			getCovers: async () => {
 				const uri = `${process.env.BACKEND_URL}/api/covers`
@@ -191,7 +201,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				}
 				const data = await response.json();
 				setStore({ covers: data.results })
-				if (getStore().covers.lenght <= 3){
+				if (getStore().covers.length <= 3){
 					setStore({populars: data.results})
 				} else {
 					const lastIndex = Math.floor(Math.random() * (getStore().covers.length - 2)) + 2;
